dc906be70d62372101777d5b6d1dd39ba75f87d4
Vinicius Solon Silva <64141214+vinicius-solon-silva@users.noreply.github.com>
2023-09-12 15:52:43 -0300

    Fixed vars

--- Sonarqube10.1-install-files/install_script/install-sonar-v10.sh
+++ Sonarqube10.1-install-files/install_script/install-sonar-v10.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # CREDENCIAIS DO DB DO SONAR
-$DB_USER=<username>
-$DB_PASSWD=<senha>
-$DB_IP_HOST=<IP do host>
-$DB_INSTANCE=<nome da instancia>
+DB_USER=username
+DB_PASSWD=passwd
+DB_IP_HOST=host_ip
+DB_INSTANCE=instance_name
 
 # CREDENCIAIS DO LDAP
-$ENABLE_LDAP = 0
-$LDAP_IP=<Ldap ip>
-$LDAP_PORT=<Ldap port>
-$LDAP_USER_EMAIL=<user email>
-$LDAP_PASSWD=<senha>
-$ORG_NAME = <organization name>
-$ORG_SUFFIX = <org name suffix>
+ENABLE_LDAP=0
+LDAP_IP=Ldap_ip
+LDAP_PORT=Ldap_port
+LDAP_USER_EMAIL=user_email
+LDAP_PASSWD=passwd
+ORG_NAME=organization_name
+ORG_SUFFIX=org_name_suffix
 
 # URL PARA FAZER DOWNLOAD DO .jar DO PLUGIN DE ANÁLISE DE BRANCHES
-$PLUGIN_URL=<URL>
+PLUGIN_URL=URL
 
 # CRIA GRUPO E USER SONARQUBE
 sudo mkdir /opt/sonarqube/
-cd /opt/sonarqube/
+sudo cd /opt/sonarqube/
 sudo addgroup sonarqube
 sudo useradd -M -d /opt/sonarqube/ -r -s /bin/bash sonarqube
 sudo usermod -aG sonarqube sonarqube
@@ -28,7 +28,7 @@ sudo chown -R sonarqube:sonarqube /opt/sonarqube
 sudo chmod -R u+rwx /opt/sonarqube
 
 # INSTALA PACOTES UTILIZADOS E O SONARQUBE 10.1
-sudo apt update && apt upgrade - y
+sudo apt update && apt upgrade -y
 sudo apt install net-tools wget unzip vim curl ufw openjdk-17-jdk
 sudo ufw allow 8080
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.1.0.73491.zip
@@ -40,24 +40,24 @@ sudo wget $PLUGIN_URL
 sudo mv sonarqube-community-branch-plugin-1.15.0-SNAPSHOT.jar extensions/plugins/
 
 # INSERE VARS DE CONFIG NO SONAR.PROPERTIES
-sudo echo "sonar.jdbc.username=$DB_USER" >> /conf/sonar.properties
-sudo echo "sonar.jdbc.password=$DB_PASSWD" >> /conf/sonar.properties
-sudo echo "sonar.jdbc.url=jdbc:sqlserver://$DB_IP_HOST:1434;instanceName=$DB_INSTANCE;databaseName=SONAR;integratedSecurity=false;trustServerCertificate=true" >> /conf/sonar.properties
-sudo echo "sonar.web.javaOpts=-Xmx1024m -Xms512m -XX:+HeapDumpOnOutOfMemoryError" >> /conf/sonar.properties
-sudo echo "sonar.web.javaAdditionalOpts=-javaagent:/opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.15.0-SNAPSHOT.jar=web" >> /conf/sonar.properties
-sudo echo "sonar.web.port=8080" >> /conf/sonar.properties
-sudo echo "sonar.ce.javaOpts=-Xmx4096m -Xms2048m -XX:+HeapDumpOnOutOfMemoryError" >> /conf/sonar.properties
-sudo echo "sonar.ce.javaAdditionalOpts=-javaagent:/opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.15.0-SNAPSHOT.jar=ce" >> /conf/sonar.properties
-sudo echo "sonar.search.javaOpts=-Xmx2048m -Xms2048m -XX:MaxDirectMemorySize=512m -XX:+HeapDumpOnOutOfMemoryError" >> /conf/sonar.properties
-sudo echo "sonar.security.realm=LDAP" >> /conf/sonar.properties
+sudo echo "sonar.jdbc.username=$DB_USER" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.jdbc.password=$DB_PASSWD" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.jdbc.url=jdbc:sqlserver://$DB_IP_HOST:1434;instanceName=$DB_INSTANCE;databaseName=SONAR;integratedSecurity=false;trustServerCertificate=true" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.web.javaOpts=-Xmx1024m -Xms512m -XX:+HeapDumpOnOutOfMemoryError" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.web.javaAdditionalOpts=-javaagent:/opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.15.0-SNAPSHOT.jar=web" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.web.port=8080" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.ce.javaOpts=-Xmx4096m -Xms2048m -XX:+HeapDumpOnOutOfMemoryError" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.ce.javaAdditionalOpts=-javaagent:/opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.15.0-SNAPSHOT.jar=ce" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.search.javaOpts=-Xmx2048m -Xms2048m -XX:MaxDirectMemorySize=512m -XX:+HeapDumpOnOutOfMemoryError" >> /opt/sonarqube/conf/sonar.properties
+sudo echo "sonar.security.realm=LDAP" >> /opt/sonarqube/conf/sonar.properties
 if [[ $ENABLE_LDAP -eq 1 ]]; then
-    sudo echo "ldap.url=LDAP://$LDAP_IP:$LDAP_PORT" >> /conf/sonar.properties
-    sudo echo "ldap.bindDn=$LDAP_USER_EMAIL" >> /conf/sonar.properties
-    sudo echo "ldap.bindPassword=$LDAP_PASSWD" >> /conf/sonar.properties
-    sudo echo "ldap.user.baseDn=DC=$ORG_NAME,DC=$ORG_SUFFIX" >> /conf/sonar.properties
-    sudo echo "ldap.user.request=(&(objectClass=user)(sAMAccountName={login}))" >> /conf/sonar.properties
-    sudo echo "ldap.user.realNameAttribute=cn" >> /conf/sonar.properties
-    sudo echo "ldap.user.emailAttribute=email" >> /conf/sonar.properties
+    sudo echo "ldap.url=LDAP://$LDAP_IP:$LDAP_PORT" >> /opt/sonarqube/conf/sonar.properties
+    sudo echo "ldap.bindDn=$LDAP_USER_EMAIL" >> /opt/sonarqube/conf/sonar.properties
+    sudo echo "ldap.bindPassword=$LDAP_PASSWD" >> /opt/sonarqube/conf/sonar.properties
+    sudo echo "ldap.user.baseDn=DC=$ORG_NAME,DC=$ORG_SUFFIX" >> /opt/sonarqube/conf/sonar.properties
+    sudo echo "ldap.user.request=(&(objectClass=user)(sAMAccountName={login}))" >> /opt/sonarqube/conf/sonar.properties
+    sudo echo "ldap.user.realNameAttribute=cn" >> /opt/sonarqube/conf/sonar.properties
+    sudo echo "ldap.user.emailAttribute=email" >> /opt/sonarqube/conf/sonar.properties
 fi
 
 # CRIA E CONFIGURA O ARQUIVO DO SYSTEMCTL
